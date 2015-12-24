bcb06e094a0aafc89b75bdc78effec2dbe267243
Billy Gottenstrater <celestite4@yahoo.com>
2015-12-24 10:03:05 -0500

    The button changes the bookmark 190 to the current tab's url. Still working on getting the user to be able to select a different one.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Bookmark Changer Chrome Extension
+## End Goal:
+To create a Google Chrome extension that lets the user select a given bookmark. Every time the user clicks on this app's icon, it will update the url of that bookmark to the url of that tab.
+##Why?
+* So I can keep track of what strip of a webcomic I am on.
+* Improve Javascript skills
+* Learn more about Google's API
+* Learn how to create Chrome extensions
--- popup.js
+++ popup.js
@@ -47,72 +47,29 @@ function getCurrentTabUrl(callback) {
   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
-}
-
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
-
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
+function printBookmarks(id, array, callback) {
+    chrome.bookmarks.getChildren(id, function(children) {
+      children.forEach(function(bookmark) { 
+        //console.debug(bookmark.title+" "+bookmark.id+ " | ");
+        array.push({title:String(bookmark.title),id:String(bookmark.id)});
+        printBookmarks(bookmark.id,array,callback);
+      });
     });
+    callback(array);
+  }
+
+function showBookmarks(id){
+  var arr = [];
+  printBookmarks(id, arr, function(array){
+    array.forEach(function(element){
+    console.debug("Title: "+element.title+" ID: "+element.id)});
   });
-});
+}
+
+function changeBookmark(id){
+  getCurrentTabUrl(function(url){
+    chrome.bookmarks.update(id,{url:url});
+  });
+}
+
+document.getElementById("button1").addEventListener("click",changeBookmark("190"));
